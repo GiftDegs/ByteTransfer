a2d711000b7277f192d2a35f52c99594ad5a8e02
David <degstrader@gmail.com>
2026-05-03 21:13:35 -0300

    Eliminar cotizador publico legacy

--- public/src/main.js
+++ public/src/main.js
@@ -1,24 +1,16 @@
 ﻿import { enableSharePreviewTools } from "./ui/sharePreview.js";
-import { DOM } from "./ui/dom.js";
 import { initRipple } from "./ui/ripple.js";
 import { renderQuoteScreen } from "./ui/quoteScreens.js";
 
 function iniciarCotizadorPublico() {
-  DOM.mainHeader?.classList.add("hidden");
-
   const app = document.getElementById("app");
+  const quoteApp = document.getElementById("quoteApp");
+
   app?.classList.remove("justify-center");
   app?.classList.add("justify-start");
 
-  DOM.step1Origen?.classList.add("hidden");
-  DOM.step2Destino?.classList.add("hidden");
-  DOM.tasaWrap?.classList.add("hidden");
-  DOM.step1?.classList.add("hidden");
-  DOM.step2?.classList.add("hidden");
-  DOM.resultado?.classList.add("hidden");
-
-  DOM.quoteApp?.classList.remove("hidden");
-  renderQuoteScreen(DOM.quoteApp);
+  quoteApp?.classList.remove("hidden");
+  renderQuoteScreen(quoteApp);
 }
 
 function shouldEnableSharePreviewTools() {
