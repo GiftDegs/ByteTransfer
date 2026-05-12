8ac63511feebe4b1d5bb3373d6624729ffc53d4c
David <degstrader@gmail.com>
2026-05-12 07:06:26 -0300

    Limpiar aliases muertos del Core Console

--- apps/core-console/app.js
+++ apps/core-console/app.js
@@ -1,57 +1,8 @@
 ﻿const dhemkaState = window.dhemkaState;
-const escapeHtml = window.escapeHtml;
 const getTenants = window.getTenants;
-const getActiveTenant = window.getActiveTenant;
-
-const getTenantStatusStyles = window.getTenantStatusStyles;
-const getModuleStatusStyles = window.getModuleStatusStyles;
-const getEnabledModules = window.getEnabledModules;
-const getAllModules = window.getAllModules;
-const getTenantMetrics = window.getTenantMetrics;
-const renderModulePills = window.renderModulePills;
-const renderModuleRows = window.renderModuleRows;
-const buildAccessSurfaces = window.buildAccessSurfaces;
-const renderAccessSurfaceRows = window.renderAccessSurfaceRows;
-
-const getOperationalBase = window.getOperationalBase;
-const getOperationalStatusStyles = window.getOperationalStatusStyles;
-const renderOperationalStatusPill = window.renderOperationalStatusPill;
-const renderOperationalReferenceRows = window.renderOperationalReferenceRows;
-const renderOperationalProviderRows = window.renderOperationalProviderRows;
-const renderCoreControlRows = window.renderCoreControlRows;
-const renderOperationalBasePanel = window.renderOperationalBasePanel;
-
-const getQuoteEngine = window.getQuoteEngine;
-const getQuoteStatusStyles = window.getQuoteStatusStyles;
-const renderQuoteStatusPill = window.renderQuoteStatusPill;
-const renderQuoteProviderRows = window.renderQuoteProviderRows;
-const renderQuoteStrategyRows = window.renderQuoteStrategyRows;
-const renderQuoteFallbackRows = window.renderQuoteFallbackRows;
-const renderTenantBoundaryRows = window.renderTenantBoundaryRows;
-const renderQuoteEnginePanel = window.renderQuoteEnginePanel;
-
-const getMonitoring = window.getMonitoring;
-const getMonitoringStatusStyles = window.getMonitoringStatusStyles;
-const renderMonitoringStatusPill = window.renderMonitoringStatusPill;
-const renderMonitoringWorkerRows = window.renderMonitoringWorkerRows;
-const renderMonitoringHealthRows = window.renderMonitoringHealthRows;
-const getAlertSeverityStyles = window.getAlertSeverityStyles;
-const renderMonitoringAlertRows = window.renderMonitoringAlertRows;
-const renderMonitoringPanel = window.renderMonitoringPanel;
-
 const setActiveSidebarItem = window.setActiveSidebarItem;
-const buildTenantSectionCards = window.buildTenantSectionCards;
-const getSectionCards = window.getSectionCards;
 const renderSectionContent = window.renderSectionContent;
-const renderSectionCards = window.renderSectionCards;
-
-const renderActiveTenantCard = window.renderActiveTenantCard;
-const renderTenantSwitcher = window.renderTenantSwitcher;
-const renderCreateTenantPlaceholder = window.renderCreateTenantPlaceholder;
-const renderTenantEcosystemList = window.renderTenantEcosystemList;
-const renderTenantDetailPanel = window.renderTenantDetailPanel;
 const renderTenantSurfaces = window.renderTenantSurfaces;
-
 function setActiveTenant(tenantId) {
   const tenantExists = getTenants().some((tenant) => tenant.id === tenantId);
 
